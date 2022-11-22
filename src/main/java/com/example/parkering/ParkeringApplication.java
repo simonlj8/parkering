5e35af9286f9b8bb5299c51233ea1788a233823c
Simon Ljungström <simonlj8@gmail.com>
2022-11-22 14:53:14 +0100

    First commit

---
+++ src/main/java/com/example/parkering/ParkeringApplication.java
@@ -0,0 +1,13 @@
+package com.example.parkering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkeringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkeringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/parkering/ParkeringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parkering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkeringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
